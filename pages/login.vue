0590b876ddd1aace1ceef37496632ef1daec9910
pushu0 <pushul@gmail.com>
2022-06-25 10:36:14 +0100

    Enable store / Create login page

---
+++ pages/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-btn @click="userLogin">Login with Spotify</v-btn>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      login: {
+        username: '',
+        password: '',
+      },
+    }
+  },
+  methods: {
+    async userLogin() {
+      try {
+        await this.$auth.loginWith('custom')
+      } catch (err) {
+        console.log(err)
+      }
+    },
+  },
+}
+</script>
