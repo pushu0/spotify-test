3f490ebafd305f49bb8b60efd6c75ab50f59aa6b
pushu0 <pushul@gmail.com>
2022-06-25 10:36:22 +0100

    Handle auth on callback page

---
+++ pages/callback.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Loading</div>
+</template>
+<script>
+export default {
+  name: 'CallBackPage',
+  mounted() {
+    const { valid } = this.$auth.check()
+    if (!valid) error('Ooops, something went wrong')
+  },
+}
+</script>
