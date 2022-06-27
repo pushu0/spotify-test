997684e2e406f7781f01ef815248e7bd1b1a22b6
pushu0 <pushul@gmail.com>
2022-06-27 16:37:09 +0100

    Create artist and track components

---
+++ components/Artist.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-btn
+    text
+    class="button"
+    :class="{ selected: isSelected }"
+    @click="$emit('click')"
+  >
+    <v-icon left dark>
+      {{ isSelected ? 'mdi-check' : 'mdi-plus' }}
+    </v-icon>
+    {{ name }}
+  </v-btn>
+</template>
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'SimplifiedArtist',
+  props: {
+    name: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    isSelected: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  setup() {},
+})
+</script>
+<style scoped>
+.button {
+  margin-left: 0px;
+  transition: all 0.3s;
+}
+.button.selected {
+  margin-left: 20px;
+}
+</style>
---
+++ components/Track.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card height="300" rounded="lg" light>
+    <v-img
+      :src="item.imageUrl"
+      transition="fade-transition"
+      height="300"
+      class="grey darken-4 scaled"
+      content-class="content"
+    >
+      <div class="background">
+        <v-card-title class="subtitle-2">
+          {{ item.name }}
+        </v-card-title>
+        <v-card-subtitle class="caption">
+          By: {{ item.artists }}
+        </v-card-subtitle>
+      </div>
+    </v-img>
+  </v-card>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from '@nuxtjs/composition-api'
+import { SimplifiedTrack } from '~/types'
+
+export default defineComponent({
+  name: 'SimplifiedTrack',
+
+  props: {
+    item: {
+      type: Object as PropType<SimplifiedTrack>,
+      required: false,
+      default: () => ({
+        name: '',
+        artists: '',
+        imageUrl: '',
+      }),
+    },
+  },
+  setup() {},
+})
+</script>
+<style scoped>
+::v-deep .content {
+  position: relative;
+}
+.background {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: white;
+}
+::v-deep .scaled .v-image__image {
+  transform: scale(1.1);
+}
+</style>
